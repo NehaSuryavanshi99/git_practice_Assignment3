6cbfb6a5b90b6c9fd4992c49a8397402248aa207
NehasSuryavanshi99 <nehasuryavanshi121999@gmail.com>
2022-11-06 15:25:04 +0530

    add

--- prime.js
+++ prime.js
@@ -1,15 +1,15 @@
 function Prime(num) {
     // Write code here
-        let iprime = "Yes";
+        let isprime = "Yes";
     
     if(num == 1){
         isprime = "No";
     }else if(num == 2){
         isprime = "Yes";
     }else if(num === 3){
-         isprime = "Yes";
+         isprime == "Yes";
     }else{
-        for(let x = 2 ; x <= num ; x++){
+        for(let x = 2 ; x < num ; x++){
             if(num % x == 0){
                 isprime = "No";
                 
