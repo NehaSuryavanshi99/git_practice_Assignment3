ea092f0fc88024759254cbb8adb7b08edd7328a2
shubham587 <shubhampwr9945@gmail.com>
2022-11-06 15:25:12 +0530

    code resolved

--- prime.js
+++ prime.js
@@ -1,6 +1,6 @@
 function Prime(num) {
     // Write code here
-        let iprime = "Yes";
+        let isprime = "Yes";
     
     if(num == 1){
         isprime = "No";
@@ -9,7 +9,7 @@ function Prime(num) {
     }else if(num === 3){
          isprime = "Yes";
     }else{
-        for(let x = 2 ; x <= num ; x++){
+        for(let x = 2 ; x < num ; x++){
             if(num % x == 0){
                 isprime = "No";
                 
